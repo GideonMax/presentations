b980230477997f9f3962d5cf4949dbf77ae59d92
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-01-07 19:26:18 +0200

    Update jqueryserver.js
    g

--- jqueryserver.js
+++ jqueryserver.js
@@ -7,19 +7,19 @@ app.get("/click", (req, res) => {//9
     res.sendFile(__dirname + "/click.html");
 })
 
-app.get("/:name.load", (req, res) => {
+app.get("/:name.load", (req, res) => {//10
     res.sendFile(__dirname + "/load/" + req.params.name + ".html")
 })
 
-app.get("/jquerydata", (req, res) => {//10,11
+app.get("/jquerydata", (req, res) => {//10,11,12
     res.sendFile(__dirname + "/jquery_data.html")
 })
-app.post("/post",(req,res)=>{
+app.post("/post",(req,res)=>{//12
     console.log("a");
     
     console.log(req.body.data );
 })
-app.get("/example.dat", (req, res) => {
+app.get("/example.dat", (req, res) => {//11
     res.send("data");
 })
 
