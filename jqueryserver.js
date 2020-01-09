95664a5164215cdf9eab19d58d1be10d49ac2b96
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-01-09 16:32:01 +0200

    g

--- jqueryserver.js
+++ jqueryserver.js
@@ -2,27 +2,26 @@ const express = require("express");
 const app = express();
 const BodyParse = require("body-parser");
 app.use(BodyParse.urlencoded({ extended: true }));
-app.get("/click", (req, res) => {//9
+app.get("/click", (req, res) => { //9
     res.sendFile(__dirname + "/click.html");
 })
 
-app.get("/:name.load", (req, res) => {//10
+app.get("/:name.load", (req, res) => { //10
     res.sendFile(__dirname + "/load/" + req.params.name + ".html")
 })
 
-app.get("/jquerydata", (req, res) => {//10,11,12
+app.get("/jquerydata", (req, res) => { //10,11,12
     res.sendFile(__dirname + "/jquery_data.html")
 })
-app.post("/post",(req,res)=>{//12
-    console.log("a");
-    
-    console.log(req.body.data );
+app.post("/post", (req, res) => { //12
+
+    console.log(req.body.data);
 })
-app.get("/example.dat", (req, res) => {//11
+app.get("/example.dat", (req, res) => { //11
     res.send("data");
 })
 
-app.get("/yes", (req, res) => {//7,8
+app.get("/yes", (req, res) => { //7,8
     res.sendFile(__dirname + "/jquery_toggle.html");
 })
 app.listen(3000)
